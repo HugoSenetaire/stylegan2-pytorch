bdfff34d4faad3117e7a078f1c610defc89bb26e
Boombot01 <hugo.senetaire@eleves.enpc.fr>
2020-07-29 14:31:23 +0200

    Add data sampler

--- inspirationnal_generation.py
+++ inspirationnal_generation.py
@@ -50,6 +50,17 @@ def pil_loader(path):
 
 
 
+def data_sampler(dataset, shuffle, distributed):
+    if distributed:
+        return data.distributed.DistributedSampler(dataset, shuffle=shuffle)
+
+    if shuffle:
+        return data.RandomSampler(dataset)
+
+    else:
+        return data.SequentialSampler(dataset)
+
+        
 def gradientDescentOnInput(model,
                            input,
                            featureExtractors,
