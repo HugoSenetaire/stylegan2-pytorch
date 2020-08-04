1a8c548199deb88e5d607cfeea203b65cd4debbd
HugoSenetaire <hugo.senetaire@gmail.com>
2020-08-04 18:19:28 +0200

    debug

--- model.py
+++ model.py
@@ -663,7 +663,8 @@ class Generator(nn.Module):
         for conv1, conv2, noise1, noise2, to_rgb in zip(
             self.convs[::2], self.convs[1::2], noise[1::2], noise[2::2], self.to_rgbs
         ):  
-            
+            print(noise1)
+            print(noise2)
             out = conv1(out, latent[:, i], noise=noise1)
             out = conv2(out, latent[:, i + 1], noise=noise2)
             skip = to_rgb(out, latent[:, i + 2], skip)
