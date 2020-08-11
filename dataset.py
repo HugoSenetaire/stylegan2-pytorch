356510e35d512ed085dae14ddc9a8b11b62292fe
HugoSenetaire <hugo.senetaire@gmail.com>
2020-08-11 15:54:27 +0200

    fix typo

--- dataset.py
+++ dataset.py
@@ -248,7 +248,7 @@ class Dataset(data.Dataset):
 
         
         possibleLength = len(self.dic_inspirationnal[self.columns_inspirationnal[0]])
-        weights = torch.zeros((possibleLength,),dtype = float64)
+        weights = torch.zeros((possibleLength,),dtype = torch.float64)
         weights.new_full((possibleLength,),1./possibleLength)
         dic_weights = {self.columns_inspirationnal[0] : weights}
         one_hot = weights
@@ -256,7 +256,7 @@ class Dataset(data.Dataset):
             if i == 0 :
                 continue
             possibleLength = len(self.dic_inspirationnal[column])
-            weights = torch.zeros((possibleLength,),dtype = float64)
+            weights = torch.zeros((possibleLength,),dtype = torch.float64)
             weights.new_full((possibleLength,),1./possibleLength)
             dic_weights[column] = [weights]
             one_hot = torch.cat((one_hot,weights))
@@ -265,7 +265,7 @@ class Dataset(data.Dataset):
         one_hot = one_hot[None,:]
         for k in range(1,batch_size):
             possibleLength = len(self.dic_inspirationnal[self.columns_inspirationnal[0]])
-            weights = torch.zeros((possibleLength,),dtype = float64)
+            weights = torch.zeros((possibleLength,),dtype = torch.float64)
             weights.new_full((possibleLength,),1./possibleLength)
             dic_weights = {self.columns_inspirationnal[0] : weights}
             aux_one_hot = weights
@@ -274,7 +274,7 @@ class Dataset(data.Dataset):
                 if i == 0 :
                     continue
                 possibleLength = len(self.dic_inspirationnal[column])
-                weights = torch.zeros((possibleLength,),dtype = float64)
+                weights = torch.zeros((possibleLength,),dtype = torch.float64)
                 weights = torch.new_full((possibleLength,),1./possibleLength)
                 dic_weights[column] = [weights]
                 aux_one_hot = torch.cat((aux_one_hot,weights))
