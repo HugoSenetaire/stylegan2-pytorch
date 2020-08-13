4d40a2736438b297c29db21fd6833b0652cb9693
HugoSenetaire <hugo.senetaire@gmail.com>
2020-08-13 17:26:38 +0200

    fix loader for optimiser

--- train.py
+++ train.py
@@ -581,8 +581,20 @@ if __name__ == "__main__":
         discriminator.load_state_dict(ckpt["d"])
         g_ema.load_state_dict(ckpt["g_ema"])
 
-        g_optim.load_state_dict(ckpt["g_optim"])
-        d_optim.load_state_dict(ckpt["d_optim"])
+        try :
+            g_optim.load_state_dict(ckpt["g_optim"])
+            d_optim.load_state_dict(ckpt["d_optim"])
+        except :
+            g_optim = optim.Adam(
+            generator.parameters(),
+            lr=args.lr * g_reg_ratio,
+            betas=(0 ** g_reg_ratio, 0.99 ** g_reg_ratio),
+            )
+            d_optim = optim.Adam(
+                discriminator.parameters(),
+                lr=args.lr * d_reg_ratio,
+                betas=(0 ** d_reg_ratio, 0.99 ** d_reg_ratio),
+            )
 
     if args.distributed:
         generator = nn.parallel.DistributedDataParallel(
