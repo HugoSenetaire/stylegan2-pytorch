f675427b344929b9bb76b012f55ba98ee524d351
HugoSenetaire <hugo.senetaire@gmail.com>
2020-08-13 11:12:52 +0200

    add debug option

--- train.py
+++ train.py
@@ -221,6 +221,7 @@ def train(args, loader, dataset, generator, discriminator, g_optim, d_optim, g_e
             if i%args.upscale_every == 0 and dataset.image_size<args.max_size:
                 print(f"Upscale at {i}")
                 print(f"next upscale at {args.upscale_every*2}")
+                print(f"New size is {dataset.image_size}")
                 args.upscale_every = args.upscale_every*2
                 add_scale(dataset,generator,discriminator,g_ema,g_optim,d_optim,device)
 
