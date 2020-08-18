7c381736a495acadd375824ae7603af1262ee872
HugoSenetaire <hugo.senetaire@gmail.com>
2020-08-18 12:01:59 +0200

    change resize for inspirationnal loss

--- train.py
+++ train.py
@@ -104,7 +104,7 @@ def creativity_loss(pred,weights,device):
     batch,column_size = weights.shape
     pred_aux = pred.unsqueeze(1)
     pred_aux = pred_aux.expand(-1,column_size,-1)
-    pred_aux = pred_aux.view(batch*column_size,column_size)
+    pred_aux = pred_aux.reshape(batch*column_size,column_size)
     neo_labels = create_label(batch,column_size,device)
     weights_aux = weights.flatten()
     pred_output = torch.nn.functional.cross_entropy(pred_aux,neo_labels, reduce=False)
