4cd3b06f0ca1a65310db3142bd84139d2ee58611
HugoSenetaire <hugo.senetaire@gmail.com>
2020-09-03 15:33:06 +0200

    test

--- dataset.py
+++ dataset.py
@@ -186,9 +186,9 @@ class Dataset(data.Dataset):
         path_mask = os.path.join(self.folder_mask,name+".jpg")
         if self.transform_mask is not None :
             mask = Image.open(path_mask).convert('L')
-            print(mask.shape)
+            print(mask.shape())
             mask_transform = self.transform_mask(mask)
-            print(mask_transform)
+            print(mask_transform.shape)
         else :
             mask_transform = None
     
