37c2a43058cc1b44f4a4acfc3067a9f47bcf244f
Boombot01 <hugo.senetaire@eleves.enpc.fr>
2020-09-06 20:48:42 +0200

    debug

--- fid.py
+++ fid.py
@@ -33,7 +33,7 @@ def extract_feature_from_samples(
     batch_sizes = [batch_size] * n_batch + [resid]
     features = []
 
-    for batch in tqdm(range(len(batch_sizes)-1)):
+    for i in tqdm(range(len(batch_sizes)-1)):
         batch = batch_sizes[i]
         latent = torch.randn(batch, 512, device=device)
         if label_name is None or label_name == 'None':
