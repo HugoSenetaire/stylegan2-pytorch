b7574acf7b168e737a8d6dfb4c978f38dbad83ce
Boombot01 <hugo.senetaire@eleves.enpc.fr>
2020-09-06 17:38:34 +0200

    dataset variation

--- calc_inception.py
+++ calc_inception.py
@@ -66,7 +66,7 @@ def extract_features(loader, inception, device):
 
     feature_list = []
 
-    for _,img,_,_ in pbar:
+    for _,img,_,_,_ in pbar:
         img = img.to(device)
         feature = inception(img)[0].view(img.shape[0], -1)
         feature_list.append(feature.to('cpu'))
