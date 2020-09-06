29bc25b86c586455accc3df8cfe76407058a80e8
Boombot01 <hugo.senetaire@eleves.enpc.fr>
2020-09-06 18:01:08 +0200

    error

--- parser_utils/parser_dataset.py
+++ parser_utils/parser_dataset.py
@@ -10,7 +10,6 @@ def create_parser_dataset(parser):
     parser.add_argument("--multiview", action = "store_true")
     parser.add_argument("--labels", nargs='*', help='List of element used for classification', type=str, default = [])
     parser.add_argument("--labels_inspirationnal", nargs='*', help='List of element used for inspiration algorithm',type=str, default = [])
-    parser.add_argument("--size", type=int, default=256)
     parser.add_argument("--csv_path", type = str, default = None)  
     parser.add_argument("--inspiration_method", type=str, default = "fullrandom", help = "Possible value is fullrandom/onlyinspiration") 
     parser.add_argument("--label_method", type=str, default = "listing", help = "Possible value is random/listing")  
